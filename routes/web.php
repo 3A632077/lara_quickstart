b39bc37e41ead328ad6b70b5ba722eb0644b0f34
林威震 <weizhen19990727@gmail.com>
2019-10-14 21:35:46 +0800

    新增路由一、二功能 並新增tasks.blade.php和app.blades.php和error.blade.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Task;
+use Illuminate\Http\Request;
+//顯示所有任務的清單
+Route::get('/', function () {
+//顯示我們所有任務的清單
+    Route::get('/', function () {
+        return view('tasks');
+    });
+
+});
+// 增加新的任務
+Route::post('/task', function (Request $request) {
+    // 驗證輸入
+    $validator = Validator::make($request->all() , [
+        'name' => 'required|max:255',
+    ]);
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    // 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+    // 建立該任務...
+//新增任務存入DB的程式碼 (see next page)
+});
+// 刪除任務
+Route::delete('/task/{task}', function (Task $task) {
+    //
+});
